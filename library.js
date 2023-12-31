53d5dc5b43bbe0ff149a0438c0b0664701daf766
Mitchell <72991363+1q2a1@users.noreply.github.com>
2023-12-31 13:45:25 +0800

    added add book function and modal

--- library.js
+++ library.js
@@ -11,37 +11,62 @@ function Book(title, author, pages, read) {
     }
 }
 
-const hobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false)
-console.log(hobbit.info())
-
 function addBookToLibrary(title, author, pages, read) {
      const newBook = new Book(title, author, pages, read)
      myLibrary.push(newBook)
 }
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false)
-addBookToLibrary("The Glove", "J.R.R Tolkien", 312, false)
-addBookToLibrary("Ruby", "Roald Dahl", 317, false)
-addBookToLibrary("Running Late", "Steven Lim", 512, true)
-console.log(myLibrary)
 
 function displayBooks(){
     let container = document.getElementsByClassName("library")
     if (container.length > 0){
         container = container[0]
-
+        container.innerHTML = ''
         myLibrary.forEach(function(book){
             let bookElement = document.createElement("div")
             bookElement.className= "book"
             bookElement.innerHTML = `
-                <h3>${book.title}</h3>
-                <p>Author: ${book.author}</p>
-                <p>Pages: ${book.pages}</p>
-                <p>${book.read ? "Read" : "Not Read"}</p>
+            <h3>${book.title}</h3>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>${book.read ? "Read" : "Not Read"}</p>
             `
             container.appendChild(bookElement)
         }) 
     }
 }
 
+document.getElementById("addBookButton").addEventListener("click", function() {
+    const formSection = document.getElementById("form").style.display = "flex"
+    const contentSection = document.getElementById("content").style.display = "none"
+})
+
+document.getElementById("submit-btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").value;
+    
+    read = read === "true"
+
+    const formSection = document.getElementById("form").style.display = "none"
+    addBookToLibrary(title, author, pages, read)
+    displayBooks()
+    const contentSection = document.getElementById("content").style.display = "block"
+})
+
+document.getElementById("close-btn").addEventListener("click", function(e) {
+    e.preventDefault()
+    const formSection = document.getElementById("form").style.display = "none"
+    const contentSection = document.getElementById("content").style.display = "block"
+})
+
+
+// **********************************
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, false)
+addBookToLibrary("The Glove", "J.R.R Tolkien", 312, false)
+addBookToLibrary("Ruby", "Roald Dahl", 317, false)
+addBookToLibrary("Running Late", "Steven Lim", 512, true)
+console.log(myLibrary)
 displayBooks()
